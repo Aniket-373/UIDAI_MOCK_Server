d08e3533047df2d7eb1604d2996b058035c7b147
Aniket.Bhoir <Aniket.Bhoir@utpl.com>
2026-05-05 10:46:08 +0530

    Updated UIDAI Response

--- Models/AadhaarUser.cs
+++ Models/AadhaarUser.cs
@@ -6,6 +6,11 @@ public class AadhaarUser
     public string Gender { get; set; }
     public string Phone { get; set; }
     public string Email { get; set; }
+    public string CareOf { get; set; }      // co
+    public string House { get; set; }
+    public string Street { get; set; }
+    public string Locality { get; set; }    // loc
+    public string VillageTown { get; set; } // vtc
     public string District { get; set; }
     public string State { get; set; }
     public string Pincode { get; set; }
